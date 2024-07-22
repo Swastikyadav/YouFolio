e1a55a127a12b93f0849a02a93115bd6708d450e
Swastik Yadav <swastikb380@gmail.com>
2024-07-22 19:06:22 +0530

    Auth: Auth setup with NextAuthJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <>Home Page!</>;
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <div>
+      <form action={actions.signIn}>
+        <button>SignIn</button>
+      </form>
+      <form action={actions.signOut}>
+        <button>SignOut</button>
+      </form>
+
+      <div>{JSON.stringify(session?.user)}</div>
+    </div>
+  );
 }
