2db010e62b90df4967c5d8f42632c01e5f29fa60
Swastik Yadav <swastikb380@gmail.com>
2024-07-23 22:02:44 +0530

    Add initial inputs and forms

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,112 @@
 import * as actions from "@/actions";
 import { auth } from "@/auth";
 
+import {
+  Alert,
+  AlertTitle,
+  AlertDescription,
+  Input,
+  Label,
+  Textarea,
+  Button,
+} from "@/components/ui";
+
 import MinimalistResume from "@/components/templates/minimalistResume";
+import Image from "next/image";
+import { Terminal } from "lucide-react";
 
 export default async function Dashboard() {
   const session = await auth();
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <aside className="p-12">User Details Form</aside>
+      <aside className="p-12">
+        <Image src="/images/folio.png" width={100} height={100} alt="logo" />
+        <Alert>
+          <Terminal className="h-4 w-4" />
+          <AlertTitle>Heads up!</AlertTitle>
+          <AlertDescription>
+            Right now there is only one template available, more templates
+            comming soon. Preview is on the right.
+          </AlertDescription>
+        </Alert>
+        <h2 className="text-xl font-medium my-4">Edit Your Details</h2>
+
+        {/* TODO: Move Form into separate component */}
+        <form action="" className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="name">Full Name</Label>
+          <Input type="text" id="name" placeholder="Full Name" />
+
+          <Label htmlFor="imageUrl" className="mt-2">
+            Image URL
+          </Label>
+          <Input
+            type="text"
+            id="imageUrl"
+            placeholder="Ex: https://someurl.com/profilepic.png"
+          />
+
+          <Label htmlFor="special" className="mt-2">
+            Specialization
+          </Label>
+          <Input
+            type="text"
+            id="special"
+            placeholder="Ex: FrontEnd, BackEnd, FullStack Software Engineer"
+          />
+
+          <Label htmlFor="skills" className="mt-2">
+            Skills
+          </Label>
+          <Input
+            type="text"
+            id="skills"
+            placeholder="Ex: JavaScript, React, NextJs, Node"
+          />
+          <small className="text-slate-500">
+            Add 4-5 top skills. Separated by comma.
+          </small>
+
+          <Label htmlFor="githubUrl" className="mt-2">
+            GitHub Link
+          </Label>
+          <Input
+            type="text"
+            id="githubUrl"
+            placeholder="Ex: https://github.com/swastikyadav"
+          />
+
+          <Label htmlFor="linkedinUrl" className="mt-2">
+            LinkedIn Link
+          </Label>
+          <Input
+            type="text"
+            id="linkedinUrl"
+            placeholder="Ex: https://linkedin/in/swastikyadav"
+          />
+
+          <Label htmlFor="email" className="mt-2">
+            Email
+          </Label>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Ex: something@gmail.com"
+          />
+
+          <Label htmlFor="about" className="mt-2">
+            About
+          </Label>
+          <Textarea
+            id="about"
+            placeholder="Brag about your achievements here..."
+          />
+
+          <Button type="submit" className="mt-4 bg-blue-600">
+            Publish & Go Live!
+          </Button>
+        </form>
+      </aside>
       <aside className="p-12">
         <MinimalistResume />
       </aside>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,6 @@
 export * from "./badge";
 export * from "./button";
+export * from "./alert";
+export * from "./input";
+export * from "./label";
+export * from "./textarea";
